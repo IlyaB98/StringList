8675410650daa656dcbafbac2c93c97e0f0a3396
Ilya <b.ilya@mail.ru>
2022-12-07 16:00:51 +0300

    Выполнил дз по алгоритмам (часть 1)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List list = new List(3);
+        List list2 = new List(3);
+
+        list2.add("String 1");
+        list2.add("String 3");
+
+        System.out.println(list.add("String 1"));
+        System.out.println(list.add(1,"String 2"));
+        System.out.println(list.set(1,"String 3"));
+//        System.out.println(list.remove("String 3"));
+//        System.out.println(list.remove(1));
+        System.out.println(list.contains("String 2"));
+        System.out.println(list.indexOf("String 1"));
+        System.out.println(list.lastIndexOf("String 3"));
+        System.out.println(list.get(0));
+        System.out.println(list.equals(list2));
+        System.out.println(list.size());
+        System.out.println(list.isEmpty());
+//        list.clear();
+        System.out.println(Arrays.toString(list.toArray()));
+
     }
 }
